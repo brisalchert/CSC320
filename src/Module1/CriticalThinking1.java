09f00137f4984b0a13acc6dbd9d65dcaab4ced58
Brian Salchert <brisalchert@gmail.com>
2024-01-17 15:20:03 -0500

    Added CriticalThinking1.java

---
+++ src/Module1/CriticalThinking1.java
@@ -0,0 +1,21 @@
+//----------------------------------------------------------------------------------------------------------------------
+//  Option #1: List Individual's Name and Address Information
+//  Demonstrate understanding of basic programming concepts by:
+//
+//  Develop pseudocode that prints the following information for a fictional person:
+//    - First name
+//    - Last name
+//    - Street address
+//    - City
+//    - Zip code
+//
+//  Create a simple java application that will print the items listed above on individual lines.
+//----------------------------------------------------------------------------------------------------------------------
+
+package Module1;
+
+public class CriticalThinking1 {
+    public static void main(String[] args) {
+        
+    }
+}

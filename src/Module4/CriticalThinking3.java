799b4fdad2a7f7840a903d1f5258c32446e97a75
Brian Salchert <brisalchert@gmail.com>
2024-02-07 14:08:58 -0500

    Added CriticalThinking3.java

---
+++ src/Module4/CriticalThinking3.java
@@ -0,0 +1,16 @@
+//----------------------------------------------------------------------------------------------------------------------
+//  Option #2: Capture Grade Statistics Using For-loop
+//  --------------------------------------------------
+//  Write a program that will provide important statistics for the grades in a class. The program will utilize a
+//  for-loop to read ten floating-point grades from user input. Include code to prevent an endless loop. Ask the user
+//  to enter the values, then print the following data:
+//
+//  - Average
+//  - Maximum
+//  - Minimum
+//----------------------------------------------------------------------------------------------------------------------
+
+package Module4;
+
+public class CriticalThinking3 {
+}

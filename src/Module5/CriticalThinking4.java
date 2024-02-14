82011cd0a8e78bab83bd047a1bb95ecc893b77d1
Brian Salchert <brisalchert@gmail.com>
2024-02-14 14:17:04 -0500

    Initialized ArrayLists for days and temperatures

--- src/Module5/CriticalThinking4.java
+++ src/Module5/CriticalThinking4.java
@@ -10,8 +10,32 @@
 
 package Module5;
 
+import java.util.ArrayList;
+import java.util.Random;
+
 public class CriticalThinking4 {
+    public static void getTemperatureReadings() {
+        Random generator = new Random();
+        ArrayList<String> weekdays = new ArrayList<>();
+        ArrayList<Double> weeklyTemperatures = new ArrayList<>();
+
+        // Add all the weekdays (lowercase so that input can be case-insensitive by setting it to lowercase)
+        weekdays.add("monday");
+        weekdays.add("tuesday");
+        weekdays.add("wednesday");
+        weekdays.add("thursday");
+        weekdays.add("friday");
+        weekdays.add("saturday");
+        weekdays.add("sunday");
+
+        // Add random weekly temperatures in the range 20.0 - 50.0 Fahrenheit
+        for (int index = 0; index < weekdays.size(); index++) {
+            // Generate random int from 0 to 300, divide by (double) 10.0, then add 20 to get temp in proper range
+            weeklyTemperatures.add((generator.nextInt(300) / 10.0) + 20);
+        }
+    }
+
     public static void main(String[] args) {
-        
+        getTemperatureReadings();
     }
 }

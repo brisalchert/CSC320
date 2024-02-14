9634b950f96783e95fdadbd61dd536dda1294d93
Brian Salchert <brisalchert@gmail.com>
2024-02-14 13:25:55 -0500

    Added CriticalThinking4.java

---
+++ src/Module5/CriticalThinking4.java
@@ -0,0 +1,17 @@
+//----------------------------------------------------------------------------------------------------------------------
+//  Option #1: Get Weekly Temperatures
+//  ----------------------------------
+//  Develop a Java program that will store data in the form of daily average temperatures for one week. Store the day
+//  and average temperature in two different arraylists. Your program should prompt the user for the day of the week
+//  (Monday through Sunday) and display both the day and temperature for each day. If "week" is entered, the output
+//  for your program should provide the temperature for each day and the weekly average. Use the looping and decision
+//  constructs in combination with the arrays to complete this assignment.
+//----------------------------------------------------------------------------------------------------------------------
+
+package Module5;
+
+public class CriticalThinking4 {
+    public static void main(String[] args) {
+        
+    }
+}

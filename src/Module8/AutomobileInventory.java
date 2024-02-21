ba40b8737717ac5257937293be90eea7a9d2677f
Brian Salchert <brisalchert@gmail.com>
2024-02-21 18:20:56 -0500

    Added new vehicle and printed info

--- src/Module8/AutomobileInventory.java
+++ src/Module8/AutomobileInventory.java
@@ -8,23 +8,24 @@ public class AutomobileInventory {
         // Initialize automobile inventory
         HashMap<String, Automobile> automobileInventory = new HashMap<>();
         Scanner input = new Scanner(System.in);
-        String returnMessage;
-        String[] vehicleInfo;
+        String returnMessage, firstVehicleName, secondVehicleName;
+        String[] firstVehicleInfo, secondVehicleInfo;
+        Automobile firstVehicle;
 
         // Create a vehicle using the parameterized constructor
-        Automobile firstVehicle = new Automobile("Subaru", "Impreza", "Gray", 2018, 56000);
+        firstVehicle = new Automobile("Subaru", "Impreza", "Gray", 2018, 56000);
 
         // Add the first vehicle to the inventory
-        String firstVehicleName = "Brian's car";
+        firstVehicleName = "Brian's car";
         Automobile.addVehicle(automobileInventory, firstVehicle, firstVehicleName);
 
         // List the vehicle values
-        vehicleInfo = Automobile.listVehicleInfo(automobileInventory, firstVehicleName);
+        firstVehicleInfo = Automobile.listVehicleInfo(automobileInventory, firstVehicleName);
 
         System.out.println("Vehicle Info for " + firstVehicleName + ":");
         System.out.println("------------------------------");
 
-        for (String value : vehicleInfo) {
+        for (String value : firstVehicleInfo) {
             System.out.println("\t" + value);
         }
 
@@ -33,5 +34,26 @@ public class AutomobileInventory {
         // Remove the vehicle and print the return value
         returnMessage = Automobile.removeVehicle(automobileInventory, firstVehicleName);
         System.out.println(returnMessage);
+
+        System.out.println();
+
+        // Add new vehicle to the inventory and print return value
+        secondVehicleName = Automobile.getNextVehicleName();
+        returnMessage = Automobile.addVehicle(automobileInventory, secondVehicleName);
+        System.out.println(returnMessage);
+
+        System.out.println();
+
+        // Print new vehicle information
+        secondVehicleInfo = Automobile.listVehicleInfo(automobileInventory, secondVehicleName);
+
+        System.out.println("Vehicle Info for " + secondVehicleName + ":");
+        System.out.println("------------------------------");
+
+        for (String value : secondVehicleInfo) {
+            System.out.println("\t" + value);
+        }
+
+        System.out.println();
     }
 }
